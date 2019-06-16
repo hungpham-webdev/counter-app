3d57f9ac42bb8e8868b2376e9899a0cf45ee471c
Hung Pham <hgn.pham@gmail.com>
2019-06-16 15:40:17 -0700

    add counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,9 +18,14 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement() {
-    console.log("Increment clicked");
-  }
+  //constructor() {
+  //  super();
+  //  this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     let classes = this.getBadgeClasses();
@@ -29,14 +34,11 @@ class Counter extends Component {
       <div>
         <span className={classes}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
       </div>
     );
   }
